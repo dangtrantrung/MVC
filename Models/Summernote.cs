0b8d3edf1f21e5772626f3266fabd700ceaa0ef5
dangtrantrung <trung280584@gmail.com>
2023-01-25 14:01:52 +0700

    Tích hợp Summernote

---
+++ Models/Summernote.cs
@@ -0,0 +1,26 @@
+namespace App.Models
+{
+   public class Summernote{
+       public Summernote (string summernoteID, bool loadLibrary =true)
+       {   
+           SummernoteID=summernoteID;
+           LoadLibrary =loadLibrary;
+
+       }
+       public string SummernoteID {get;set;}
+       public bool LoadLibrary {get;set;}
+        public int height {get;set;} =120;
+         public string toolbar {get;set;} =@"
+          [
+      ['style', ['style']],
+      ['font', ['bold', 'underline', 'clear']],
+      ['color', ['color']],
+      ['para', ['ul', 'ol', 'paragraph']],
+      ['table', ['table']],
+      ['insert', ['link', 'picture', 'video']],
+      ['height],['height']],
+      ['view', ['fullscreen', 'codeview', 'help']]
+            ]
+         ";
+   }
+}
